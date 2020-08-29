a844f2b8287e3406a5cd2f70858f1884d7f01f8f
Michael Kamprath <michael@kamprath.net>
2020-08-29 16:48:15 -0700

    change data file extension for JSON Lines

--- ground-software/telemetry-service/telemetry-service.py
+++ ground-software/telemetry-service/telemetry-service.py
@@ -2,6 +2,7 @@ import json
 from bottle import route, get, post, run, request, static_file
 
 DATA_FILE_DIR = '/run/telemetry'
+DATE_FILE_EXTENSION = 'jsonl'
 
 def clean_project_name(project):
     # since project is used a a file name, remove any symbols 
@@ -11,7 +12,7 @@ def clean_project_name(project):
 def return_telemetry(project):
     cleaned_project = clean_project_name(project)
     print('Downloading telemetry file for \'{0}\''.format(cleaned_porject))
-    return static_file('{0}.data'.format(cleaned_project), root=DATA_FILE_DIR)
+    return static_file('{0}.{1}'.format(cleaned_project, DATE_FILE_EXTENSION), root=DATA_FILE_DIR)
         
 
 @post('/telemetry/<project>')
@@ -19,11 +20,11 @@ def ingest_telemetry(project):
     cleaned_project = clean_project_name(project)
     telemetry_data = request.json
     if telemetry_data is not None:
-        filename = '{0}/{1}.data'.format(DATA_FILE_DIR,cleaned_project)
+        filename = '{0}/{1}.{2}'.format(DATA_FILE_DIR, cleaned_project, DATE_FILE_EXTENSION)
         with open(filename, 'a') as f:
             f.write(json.dumps(telemetry_data)+'\n')
-        print('Received telemetry for \'{0}\': {1}'.format(cleaned_project,telemetry_data))
-    return '<p>telemetry accepted for {{project}}</p>\n'
+        print('Received telemetry for \'{0}\': {1}'.format(cleaned_project, telemetry_data))
+    return 'telemetry accepted for {0}\n'.format(cleaned_project)
 
 
 run(host='0.0.0.0', port=8000, debug=True)
