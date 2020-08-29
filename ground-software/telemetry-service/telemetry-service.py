6a8724a0368d46c29676133f7ad48c643739b1ea
Michael Kamprath <michael@kamprath.net>
2020-08-29 15:33:20 -0700

    fixed spelling errors

--- ground-software/telemetry-service/telemetry-service.py
+++ ground-software/telemetry-service/telemetry-service.py
@@ -1,5 +1,5 @@
 import json
-from bottle import route, get, post, run, template, request, static_file
+from bottle import route, get, post, run, request, static_file
 
 DATA_FILE_DIR = '/run/telemetry'
 
@@ -8,21 +8,21 @@ def clean_project_name(project):
     return "".join(filter(str.isalnum, project))
          
 @get('/telemetry/<project>')
-def return_telemtry(project):
-    cleaned_porject = clean_project_name(project)
+def return_telemetry(project):
+    cleaned_project = clean_project_name(project)
     print('Downloading telemetry file for \'{0}\''.format(cleaned_porject))
-    return static_file('{0}.data'.format(cleaned_porject), root=DATA_FILE_DIR)
+    return static_file('{0}.data'.format(cleaned_project), root=DATA_FILE_DIR)
         
 
 @post('/telemetry/<project>')
 def ingest_telemetry(project):
-    cleaned_porject = clean_project_name(project)
+    cleaned_project = clean_project_name(project)
     telemetry_data = request.json
     if telemetry_data is not None:
-        filename = '{0}/{1}.data'.format(DATA_FILE_DIR,cleaned_porject)
+        filename = '{0}/{1}.data'.format(DATA_FILE_DIR,cleaned_project)
         with open(filename, 'a') as f:
             f.write(json.dumps(telemetry_data)+'\n')
-        print('Received telemetry for \'{0}\': {1}'.format(cleaned_porject,telemetry_data))
+        print('Received telemetry for \'{0}\': {1}'.format(cleaned_project,telemetry_data))
     return '<p>telemetry accepted for {{project}}</p>\n'
 
 
