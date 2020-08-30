b174d9244e23583517d9c38351a1a8bf29a46ef0
Michael Kamprath <michael@kamprath.net>
2020-08-29 17:25:06 -0700

    addedrequest information to data logging

--- ground-software/telemetry-service/telemetry-service.py
+++ ground-software/telemetry-service/telemetry-service.py
@@ -1,4 +1,5 @@
 import json
+import time
 from bottle import route, get, post, run, request, static_file
 
 DATA_FILE_DIR = '/run/telemetry'
@@ -19,11 +20,21 @@ def return_telemetry(project):
 def ingest_telemetry(project):
     cleaned_project = clean_project_name(project)
     telemetry_data = request.json
-    if telemetry_data is not None:
-        filename = '{0}/{1}.{2}'.format(DATA_FILE_DIR, cleaned_project, DATE_FILE_EXTENSION)
-        with open(filename, 'a') as f:
-            f.write(json.dumps(telemetry_data)+'\n')
-        print('Received telemetry for \'{0}\': {1}'.format(cleaned_project, telemetry_data))
+    if telemetry_data is None:
+        telemetry_data = ''
+    data_dict = {
+        'timestamp': time.time(),
+        'request_headers':{}, 
+        'request_url': request.url,
+        'posted_data': telemetry_data
+    }
+    for k in request.headers.keys():
+        data_dict['request_headers'][k] = request.get_header(k, '')
+    
+    filename = '{0}/{1}.{2}'.format(DATA_FILE_DIR, cleaned_project, DATE_FILE_EXTENSION)
+    with open(filename, 'a') as f:
+        f.write(json.dumps(data_dict)+'\n')
+    print('Received telemetry for \'{0}\': {1}'.format(cleaned_project, data_dict))
     return 'telemetry accepted for {0}\n'.format(cleaned_project)
 
 
